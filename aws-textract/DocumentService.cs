0c61adf9d8672279315d56e0c09e4aaaa8866c01
krishna <ksivaraj@deloitte.com>
2020-12-15 11:42:26 +0530

    read text from image

---
+++ aws-textract/DocumentService.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Amazon.Textract;
+using Amazon.Textract.Model;
+
+namespace aws_textract
+{
+    public interface IDocumentService
+    {
+        Task<IEnumerable<Block>> GetDocumentBlocks(string bucketName, string key);
+        Task<List<Block>> GetTextFromStream(IAmazonTextract textractClient, MemoryStream stream);
+    }
+
+    public class DocumentService : IDocumentService
+    {
+        private readonly IAmazonTextract _textract;
+
+        public DocumentService(IAmazonTextract textract)
+        {
+            _textract = textract;
+        }
+
+        public async Task<List<Block>> GetTextFromStream(IAmazonTextract textractClient, MemoryStream stream)
+        {
+            List<Block> result = null;
+            DetectDocumentTextRequest detectTextRequest = new DetectDocumentTextRequest()
+            {
+                Document = new Document
+                {
+                    Bytes = stream
+                }
+            };
+
+            try
+            {
+                Task<DetectDocumentTextResponse> detectTask = textractClient.DetectDocumentTextAsync(detectTextRequest);
+                DetectDocumentTextResponse detectTextResponse = await detectTask;
+
+                result = detectTextResponse.Blocks;
+                //PrintBlockDetails(result);
+            }
+            catch (AmazonTextractException textractException)
+            {
+                Console.WriteLine(textractException.Message, textractException.InnerException);
+            }
+            return result;
+        }
+
+
+        public async Task<IEnumerable<Block>> GetDocumentBlocks(string bucketName, string key)
+        {
+
+            var request = new StartDocumentTextDetectionRequest
+            {
+                DocumentLocation = new DocumentLocation {S3Object = new S3Object {Bucket = bucketName, Name = key}}
+            };
+            var response = await _textract.StartDocumentTextDetectionAsync(request);
+
+            var jobId = response.JobId;
+
+
+            //Wait until job complete. Note: Normally you would seperate this to a distributed event or scheduler
+            WaitForJobCompletion(response.JobId);
+
+            //Get detection results
+            var textDetectionResponses = GetJobResults(jobId);
+
+            //Return all blocks
+            return  textDetectionResponses.SelectMany(textDetectionResponse => textDetectionResponse.Blocks);
+
+        
+        }
+
+
+
+        private void WaitForJobCompletion(string jobId, int delay = 5000)
+        {
+            while (!IsJobComplete(jobId)) Wait(delay);
+        }
+
+        private bool IsJobComplete(string jobId)
+        {
+            var response = _textract.GetDocumentTextDetectionAsync(new GetDocumentTextDetectionRequest
+            {
+                JobId = jobId
+            });
+            response.Wait();
+            return !response.Result.JobStatus.Equals("IN_PROGRESS");
+        }
+
+        private List<GetDocumentTextDetectionResponse> GetJobResults(string jobId)
+        {
+            var result = new List<GetDocumentTextDetectionResponse>();
+            var response = _textract.GetDocumentTextDetectionAsync(new GetDocumentTextDetectionRequest
+            {
+                JobId = jobId
+            });
+            response.Wait();
+            result.Add(response.Result);
+            var nextToken = response.Result.NextToken;
+            while (nextToken != null)
+            {
+                Wait();
+                response = _textract.GetDocumentTextDetectionAsync(new GetDocumentTextDetectionRequest
+                {
+                    JobId = jobId,
+                    NextToken = response.Result.NextToken
+                });
+                response.Wait();
+                result.Add(response.Result);
+                nextToken = response.Result.NextToken;
+            }
+
+            return result;
+        }
+
+        private void Wait(int delay = 5000)
+        {
+            Task.Delay(delay).Wait();
+            Console.Write(".");
+        }
+    }
+}
---
+++ aws-textract/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.Textract;
+using Amazon.Textract.Model;
+
+namespace aws_textract
+{
+    internal class Program
+    {
+        private const string BucketName = ""; //Set your S3 bucketname
+        private static readonly RegionEndpoint Region = RegionEndpoint.APSoutheast1; //Set your region
+
+        private static async Task Main(string[] args)
+        {
+            ////Set Logging 
+            AWSConfigs.LoggingConfig.LogTo = LoggingOptions.Console;
+            AWSConfigs.LoggingConfig.LogMetrics = false;
+            AWSConfigs.LoggingConfig.LogResponses = ResponseLoggingOption.OnError;
+
+
+            //Standard AWS example
+            Console.WriteLine("Extracting Example 1");
+            var example1 = await GetDocumentText(@"TheLifeAndWorkOfFredsonBrowers.jpg");
+            
+            Console.WriteLine(example1);
+
+
+
+        }
+
+        /// <summary>
+        ///     Get document text for provided file
+        ///     1. Upload file to S3 bucket
+        ///     2. Start Document Analyzation
+        ///     3. Wait for AWS to complete analyzation
+        ///     4. Return text word by word
+        /// </summary>
+        /// <param name="file">file (path) that you like to analyze</param>
+        /// <returns></returns>
+        private static async Task<string> GetDocumentText(string file)
+        {
+             var keyName = Path.GetFileName(file);
+
+
+            //Upload document
+            //IUploadService uploadService = new UploadService();
+            //await uploadService.Upload(file, keyName,
+            //    BucketName, Region);
+
+            FileStream stream = new FileStream(file, FileMode.Open, FileAccess.Read);
+            MemoryStream mem=new MemoryStream();
+            stream.Position = 0;
+            stream.CopyTo(mem);
+
+            IDocumentService documentService = new DocumentService(new AmazonTextractClient("", "", Region));
+                
+            //var blocks = await documentService.GetDocumentBlocks(BucketName, keyName);
+            var blocks2 = await documentService.GetTextFromStream(
+                new AmazonTextractClient("", "", Region),
+                mem);
+            var output = new StringBuilder();
+
+            //See documentation for blocktype reference https://docs.aws.amazon.com/textract/latest/dg/API_Block.html
+            foreach (var block in blocks2.Where(x => x.BlockType == "LINE")) //Where(x => x.BlockType == "LINE")
+                output.Append($"{block.Text} ");
+
+            return output.ToString();
+        }
+    }
+}
